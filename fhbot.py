108e1fdf0a718001baac539cd6f5e68a4cf7e37c
Tony Arch VM <antonytej@gmail.com>
2017-03-17 11:58:33 +0000

    Another time reducing commit

--- fhbot.py
+++ fhbot.py
@@ -22,18 +22,18 @@ def add_class(crnnum):
         for e in crnnum:
             try:
                     browser.switch_to.default_content()
-                    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.LINK_TEXT, 'Registration')))
+                    WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.LINK_TEXT, 'Registration')))
                     reg = browser.find_element_by_link_text('Registration')
                     browser.execute_script('arguments[0].click();', reg)
 
-                    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.LINK_TEXT, 'Add or Drop Classes')))
+                    WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.LINK_TEXT, 'Add or Drop Classes')))
                     add = browser.find_element_by_link_text('Add or Drop Classes')
                     browser.execute_script('arguments[0].click();', add)
 
                     browser.switch_to.default_content()
                     browser.switch_to.frame('content')
-                    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'dataentrytable')))
-                    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, 'term_id')))
+                    WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.CLASS_NAME, 'dataentrytable')))
+                    WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.ID, 'term_id')))
                     select = Select(browser.find_element_by_id('term_id'))
                     select.select_by_visible_text('2017 Spring Foothill')
                     submit = browser.find_element_by_class_name('dataentrytable')
@@ -41,7 +41,7 @@ def add_class(crnnum):
 
                     browser.switch_to.default_content()
                     browser.switch_to.frame('content')
-                    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, 'crn_id1')))
+                    WebDriverWait(browser, 7).until(EC.presence_of_element_located((By.ID, 'crn_id1')))
                     crn = browser.find_element_by_id('crn_id1')
                     crn.send_keys(e)
                      
